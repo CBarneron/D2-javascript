3c5cdd34b92d33983bbd47de7a12bda4174b0834
Corentin Barneron <71774149+CBarneron@users.noreply.github.com>
2022-11-03 17:55:37 +0100

    Fin cours

---
+++ D2-Javascript-Barneron/gifs.js
@@ -0,0 +1,34 @@
+//api
+let api_key="LBOBMRaoEHYXsNZSdaxIn3vV8afrmqKI"
+// variables
+
+let search =document.getElementById('search');
+let limit =document.getElementById('limit');
+let send = document.getElementById('send');
+let result =document.getElementById('result');
+let endPoint = "https://api.giphy.com/v1/gifs/search";
+
+
+send.addEventListener("click",()=>{
+let url = endPoint+ `?api_key=${api_key}&q=${search.value}&limit=${limit.value}`;
+console.log(url);
+
+
+fetch(url)
+.then(result=>{
+    return result.json()
+}).then(data=>{
+    console.log(data);
+    result.innerHTML="";
+    for(gif of data.data){
+        console.log(gif.images.original.url);
+        let liens = gif.images.original.url;
+        let alt =gif.title
+        var img= document.createElement('img');
+       
+
+     result.innerHTML +=  `<img src="${liens}" alt="${alt}">`       
+ 
+    }
+})
+});
